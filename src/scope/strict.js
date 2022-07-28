ed5624ee5e2ed202ff05a63576ebe16f89719af9
Balbin <xxbalbin@gmail.com>
2022-07-28 17:15:51 -0500

    strict file added

---
+++ src/scope/strict.js
@@ -0,0 +1,5 @@
+'use strict'
+// var pi; sin use strict, interpretaria asi automaticamente asignandole previamente el "var" para su ejecucion posterior
+pi = 3.1416;  //con la etiqueta use strict no permite "saltar" ciertas reglas
+console.log(pi);
+
