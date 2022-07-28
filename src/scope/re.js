6d360ed3daa5595a5cde11557c491da88a6c65cf
Balbin <xxbalbin@gmail.com>
2022-07-28 17:06:52 -0500

    redeclare file added

---
+++ src/scope/re.js
@@ -0,0 +1,24 @@
+var firstName;  //cuando se declara solo se le asigna un valor Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar y asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName); //shows Ana
+
+var secondName = 'David'; // declarar y asignar
+var secondName = 'Ana'; // reasignar
+console.log(secondName) // shows Ana
+
+// let variables
+
+let fruit = 'Apple'; // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+
+//let fruit = 'Banana'; // con let no se puede redeclarar
+
+// const
+
+const animal = 'dog';
+animal = 'cat'; // con const no se puede ni redeclarar ni reasignar
+
